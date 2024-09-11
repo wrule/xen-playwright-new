5c12e5fc03d44e08ab7201ee22f14166f2e0f671
jimao <shihao.gu@perfma.com>
2024-09-11 12:57:16 +0800

    基本完成

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { exec } from 'child_process';
 import crypto from 'crypto';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -10,14 +11,33 @@ function main() {
     const lang = req.body.lang ?? 'ts';
     const script = req.body.script ?? '';
     const config = req.body.config ?? '';
-    const uuid = crypto.randomUUID().toString();
+    // const uuid = crypto.randomUUID().toString();
+    const uuid = '48e54080-5b47-46f9-9b1a-3bb4823411d4';
     const scriptFileName = `scripts/${uuid}.spec.${lang}`;
     const configFileName = `scripts/${uuid}.config.${lang}`;
     const reportJsonFileName = `scripts/${uuid}.report.json`;
     const reportHtmlFileName = `scripts/${uuid}.report.html/index.html`;
-    fs.writeFileSync(scriptFileName, script, 'utf8');
-    fs.writeFileSync(configFileName, config, 'utf8');
-    res.json({ });
+    // fs.writeFileSync(scriptFileName, script, 'utf8');
+    // fs.writeFileSync(configFileName, config, 'utf8');
+    exec(`npx playwright test ${scriptFileName} --config ${configFileName}`, (error, stdout, stderr) => {
+      if (error) {
+        res.json({ uuid, success: false, message: error.message ?? 'script running error', stdout, stderr });
+        return;
+      }
+      try {
+        res.json({
+          uuid,
+          success: true,
+          object: {
+            html: fs.readFileSync(reportHtmlFileName, 'utf8'),
+            json: JSON.parse(fs.readFileSync(reportJsonFileName, 'utf8')),
+          },
+          stdout, stderr,
+        });
+      } catch (error: any) {
+        res.json({ uuid, success: false, message: error.message ?? 'report read error', stdout, stderr });
+      }
+    });
   });
   app.listen(6422);
 }
